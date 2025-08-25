e8f8ac17bbeeb720aaaba6e808f7489db3aa6c97
anirban2crypto <anirban2chakrabarti@gmail.com>
2025-08-25 13:50:31 +0530

    update

--- runtrace.sh
+++ runtrace.sh
@@ -9,11 +9,11 @@ echo "Log file: $logfile"
 #Arguments
 #1 Batch size, input number in 2^b_power: 4,8,16,32,64,128,256
 #2 Number of users(n), Default value: 8
-#3 Code_constant, Default value: 10 
+#3 Code_constant, Default value: 10 , depends on n
 #4 Coalition size, Default value: (n/2) 
 
 cargo run --example tracing  -- --nocapture 4  16 10  >> $logfile
-cargo run --example tracing  -- --nocapture 12 16 10  >> $logfile
+cargo run --example tracing  -- --nocapture 8  16 10  >> $logfile
 cargo run --example tracing  -- --nocapture 16 16 10  >> $logfile
 
 
